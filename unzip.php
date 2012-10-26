65fc20fcbe0ac03c37a06b087dae0e58e9a53038
Jean Thomas <tibounise@gmail.com>
2012-10-26 20:41:58 +0200

    Optimisations

--- unzip.php
+++ unzip.php
@@ -4,35 +4,34 @@
 // Coded by TiBounise (http://tibounise.com)
 // Released as GPL v3 software
 
-if (!$this->arguments->isParam(0)) {
+if (!$this->arguments->isParam(0))
   throw new InvalidArgumentException('Aucun fichier fourni');
-}
 
 $FileManager = $this->webos->managers()->get('File');
+$AbsoluteLocation = $this->terminal->getAbsoluteLocation($this->arguments->getParam(0));
 
-if (!$FileManager->exists($this->terminal->getAbsoluteLocation($this->arguments->getParam(0)))) {
+if (!$FileManager->exists($AbsoluteLocation))
   throw new InvalidArgumentException('Le fichier n\'existe pas !');
-}
 
-// Vérifier l'extension
-if (!preg_match('#\.zip$#',$this->arguments->getParam(0))) {
+// Check the extension
+if (!preg_match('#\.zip$#',$AbsoluteLocation))
   throw new InvalidArgumentException('Le fichier fourni ne semble pas être au format .zip');
-}
 
-// Générer les vrais chemins du fichier zip et du dossier parent
-$FilePath = $FileManager->get($this->terminal->getAbsoluteLocation($this->arguments->getParam(0)))->realpath();
-$FolderPath = preg_replace('#'.end(explode("/",$FilePath)).'$#','',$FilePath);
+// Generate the real paths
+$FilePath = $FileManager->get($AbsoluteLocation)->realpath();
+$ZipFilename = end(explode("/",$FilePath));
+$FolderPath = preg_replace('#'.$ZipFilename.'$#','',$FilePath);
 
-// Compter le temps à l'init
+// Stores the times at the beginning of the script
 $startTime = mktime();
 
-// Décompresser le zip
-$zip = new ZipArchive;
-$zipHandler = $zip->open($FilePath);
-if ($zipHandler === TRUE) {
-  $zip->extractTo($FolderPath);
-  $zip->close();
-  echo end(explode("/",$FilePath)).' a été décompressé en '.(mktime() - $startTime).' secondes.';
+// Unzip :P
+$ZipHandler = new ZipArchive;
+$zip = $zip->open($FilePath);
+if ($zip === TRUE) {
+  $ZipHandler->extractTo($FolderPath);
+  $ZipHandler->close();
+  echo $ZipFilename.' a été décompressé en '.(mktime() - $startTime).' secondes.';
 } else {
   throw new InvalidArgumentException('L\'archive n\'a pu s\'extraire');
 }
